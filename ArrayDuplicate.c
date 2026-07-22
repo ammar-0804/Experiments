0795f7f068bb4c6d3ac922d27009ebd1e0d5e544
ammar-0804 <ansariammar0804@gmail.com>
2026-07-22 11:14:10 +0530

    Refactor ArrayDuplicate.c for variable renaming

--- ArrayDuplicate.c
+++ ArrayDuplicate.c
@@ -8,48 +8,40 @@ Q. An array contains 10 elements. Given a number "x" that may occur several time
 
 int main()
 {
-    int n, i, x;
+    int size, i, dupli;
     
     printf("Enter the size of the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
+    scanf("%d", &size);
+    int arr[size];
     printf("Enter the array elements:\n");
-    for(i = 0; i < n; i++)
+    for(i = 0; i < size; i++)
     {
         scanf("%d", &arr[i]);
     }
 
     printf("Enter the value of x: ");
-    scanf("%d", &x);
+    scanf("%d", &dupli);
     int occindex=0;
     int count = 0;
     int position = 1;
 
-    for(i = 0; i < n; i++)
-    {
-        if(arr[i] == x)
+    for(i = 0; i < size; i++)
+    {       if(arr[i] == dupli)
         {
             count++;
-
             if(occindex == -1)
             {
                 position = i; 
                int occindex = i-1;
-            }
-        }
+            }  }
     }
-
     if(count > 0)
-    {
-        printf("Occurrence of %d = %d\n", x, count);
+    {    printf("Occurrence of %d = %d\n", dupli, count);
         printf("First occurrence position = %d\n", position);
-        printf("The first index of occurence is:%d",occindex);
-    }
+        printf("The first index of occurence is:%d",occindex);}
     else
     {
-        printf("%d is not present in the array.\n", x);
+        printf("%d is not present in the array.\n", dupli);
     }
 
     return 0;
